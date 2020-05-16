8cd99ad7123493b480005b166c8c3ed1cf726dd4
Lisa Hammarstrand <lisa@Lisas-Air.lan>
2020-05-16 21:06:19 +0200

    Created one post request

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose from 'mongoose'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/notes"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const Note = mongoose.model('Note', {
+  text: String,
+  createdAt: {
+    type: Date,
+    default: () => new Date()
+  }
+})
+
 const port = process.env.PORT || 8080
 const app = express()
 
@@ -16,6 +25,21 @@ app.use(bodyParser.json())
 // Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
+})
+
+app.post('/notes', async (req, res) => {
+  // Let the user only post text, not the whole body (req.body)
+  const note = new Note({ text: req.body.text })
+  await note.save()
+  res.json(note)
+})
+
+app.post('/notes', async (req, res) => {
+  // Same as above but destructured
+  const { text } = req.body
+  const note = new Note({ text })
+  await note.save()
+  res.json(note)
 })
 
 // Start the server
